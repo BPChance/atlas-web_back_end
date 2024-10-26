29140bb9c67554f7304121d6b8ea5a32e47bdecc
BPChance <bradenpchance6@gmail.com>
2024-10-26 11:28:00 -0500

    accidentally replaced iterator

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const NewArray = [];
 
+  let i = 0;
   for (const value of array) {
     NewArray[i] = appendString + value;
     i += 1;
