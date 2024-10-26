9148a0ef076a0c49b62d2aa1423d587f657cab2b
BPChance <bradenpchance6@gmail.com>
2024-10-26 11:50:27 -0500

    add createReportObject function with department count method

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,9 +2,8 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
 
-
     getNumberOfDepartments() {
-        return Object.keys(employeesList).length;
+      return Object.keys(employeesList).length;
     }
   };
 }
