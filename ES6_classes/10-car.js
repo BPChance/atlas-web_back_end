f7bc9b054ca19587c4222cfa57019435a499609b
BPChance <bradenpchance6@gmail.com>
2024-10-27 14:02:26 -0500

    Created Car class and clonecar method

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -2,7 +2,7 @@ const _brand = Symbol('brand');
 const _motor = Symbol('motor');
 const _color = Symbol('color')
 
-export class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this[_brand] = brand;
     this[_motor] = motor;
