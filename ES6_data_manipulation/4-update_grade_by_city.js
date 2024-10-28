d66051b6ae360582ed61cf396e57c282fb4d6b4c
BPChance <bradenpchance6@gmail.com>
2024-10-28 17:57:23 -0500

    function that returns array of students for a city with new grade

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
+    .filter((student) => student.location === city)
     .map((student) => {
       const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
       return {
