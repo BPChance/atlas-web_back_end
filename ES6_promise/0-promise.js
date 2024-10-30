1c1b4e6333361e3cc96c60ec2d975b47b461062f
BPChance <bradenpchance6@gmail.com>
2024-10-30 15:42:58 -0500

    returns a promise using the prototype

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,13 @@
+function getResponseFromApi() {
+  return new Promise((resolve, reject) => {
+    const success = true;
+
+    if (success) {
+      resolve("Yayyy!");
+    } else {
+      reject(new Error("Failed to get response."));
+    }
+  });
+}
+
+export default getResponseFromApi;
---
+++ ES6_promise/README.md
@@ -0,0 +1,1 @@
+unfinished promises readme
