127664956ce2c905801031e176a397c3ba731434
BPChance <bradenpchance6@gmail.com>
2024-10-30 17:51:40 -0500

    updated readme and adding all from 2-9

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const { body } = results[0];
+      const { firstName, lastName } = results[1];
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
--- ES6_promise/README.md
+++ ES6_promise/README.md
@@ -1,1 +1,2 @@
-unfinished promises readme
+# ES6 Promise
+This project demonstrates various uses of JavaScript Promises, handling asynchronous operations, and error management.
