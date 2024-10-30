4eca251b3644ef5dc883bec6032140f65bd3cbca
BPChance <bradenpchance6@gmail.com>
2024-10-30 17:58:17 -0500

    fixing 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 200,
-      body: 'Success',
-    }))
-    .catch(() => Error())
-    .finally(() => {
+    .then(() => {
       console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
     });
 }
 
