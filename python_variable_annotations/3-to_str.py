57eaaf10f2ebd6b21bcd6da0dc0b2227e2e382ee
BPChance <bradenpchance6@gmail.com>
2025-01-08 17:24:26 -0600

    type annotated function that converts a float to a str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type annotated function that takes a float and returns it as a string """
+
+
+def to_str(n: float) -> str:
+    """ converts a float (n) and returns it as a string """
+    return str(n)
