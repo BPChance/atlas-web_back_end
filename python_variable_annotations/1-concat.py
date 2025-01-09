65b782fa70aff820464ca1838e296083cba6c992
BPChance <bradenpchance6@gmail.com>
2025-01-08 18:28:32 -0600

    type annotated function concat that concatenates two strings

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+""" type annotated function that takes a str and a str as arguments and returns a concatenated string """
+
 
 def concat(str1: str, str2: str) -> str:
+    """ funtion takes two strings and returns concatenated string """
     return str1 + str2
