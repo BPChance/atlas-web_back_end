9287ae098ff22e50ac9344c204662df91efa49ac
BPChance <bradenpchance6@gmail.com>
2025-01-08 18:34:29 -0600

    type annotated function that takes a str, and an int or float and returns a tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-""" type annotated function that takes a str, and an int or float and returns a tuple """
+"""
+type annotated function that takes a str,
+and an int or float and returns a tuple
+"""
 
 
 from typing import Union
+
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
     """ function takes k as str, and v as int or float and returns a tuple """
