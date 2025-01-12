2c750491e319f533967a64298d8cbd932f2dfbb2
BPChance <bradenpchance6@gmail.com>
2025-01-12 12:54:12 -0600

    a function that takes an int and returns a asyncio.Task

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" a function that takes an int and returns a asyncio.Task """
+
+
+import asyncio
+wait_r = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ a function that takes max_delay:int and returns a asyncio.Task """
+    return asyncio.Task
