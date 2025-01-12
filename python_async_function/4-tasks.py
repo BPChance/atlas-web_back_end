60b6173a919b2a1c15bf5d48cf37be5c170c3bb6
BPChance <bradenpchance6@gmail.com>
2025-01-12 17:58:41 -0600

    spawns wait_random n times with max_delay     and returns list of delays ascending

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" async function that gathers mutiple random delays """
+
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawns wait_random n times with max_delay
+    and returns list of delays ascending
+    """
+    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
