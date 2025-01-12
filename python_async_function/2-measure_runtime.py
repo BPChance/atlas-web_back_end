e51d763ce667c587adaa31f9c44628eec68b47a9
BPChance <bradenpchance6@gmail.com>
2025-01-12 12:42:48 -0600

    function that takes n and max_delay as arguments and measures total execution time and returns the average as a float

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,13 +9,14 @@ import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 async def measure_time(n: int, max_delay: int) -> float:
     """
     takes n: int and max_delay: int and measures
     execution time and returns the average as a float
     """
     start_time = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     execution_time = end_time - start_time
     avg_time = execution_time / n
