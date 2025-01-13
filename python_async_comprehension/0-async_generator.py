e7dbd4b003ffe9a6893a7d1e900694bc8597fa2e
BPChance <bradenpchance6@gmail.com>
2025-01-12 18:21:49 -0600

    fixed return type hint

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ yields 10 numbers 1-10 at random with 1s delay """
     for _ in range(10):
         await asyncio.sleep(1)
