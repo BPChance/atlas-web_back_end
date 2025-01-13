3083f255aae6e469ad1dd944ad297ddf24a443ca
BPChance <bradenpchance6@gmail.com>
2025-01-12 19:38:07 -0600

    function that measures the total runtime of async_comprehension executed in parallel

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -16,11 +16,9 @@ async def measure_runtime() -> float:
     parallel and measure the total runtime
     """
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
     end_time = time.time()
     return end_time - start_time
