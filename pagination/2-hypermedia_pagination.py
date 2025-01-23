f89b6c5de6f8136b874f8f588a1e2f14ffb8ebee
BPChance <bradenpchance6@gmail.com>
2025-01-23 11:32:33 -0600

    module to enhance paginated dataset

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -4,9 +4,31 @@
 
 from typing import List
 import math
+import csv
+
+
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ get page of data from the dataset """
         assert isinstance(page, int) and page > 0
@@ -16,10 +38,9 @@ class Server:
         dataset = self.dataset()
         return dataset[start_index:end_index] \
             if start_index < len(dataset) else []
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ get a paginated dataset and pagination info """
-
-
         data = self.get_page(page, page_size)
 
         items = len(self.dataset())
@@ -35,9 +56,3 @@ class Server:
             "prev_page": prev_page,
             "total_pages": total_pages
         }
-
-def index_range(page: int, page_size: int) -> tuple:
-    """ calculate start and end indexes for page and page size """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
