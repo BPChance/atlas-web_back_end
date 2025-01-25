101f20da7e0877c540c0ad72d055e61793e5fcdd
BPChance <bradenpchance6@gmail.com>
2025-01-24 19:18:35 -0600

    making things fit

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,10 +6,16 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,\
-                 message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+) -> str:
     """ returns the log message obfuscated """
     # pattern for matching fields
-    pattern = r"({})=[^{}]+".format("|".join\
-                                    (re.escape(field) for field in fields), re.escape(separator))
+    pattern = r"({})=[^{}]+".format(
+        "|".join(re.escape(field) for field in fields),
+        re.escape(separator)
+    )
     return re.sub(pattern, r"\1={}".format(redaction), message)
