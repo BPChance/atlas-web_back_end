3bb82d07ada29f86129dc6cb680eb2e116c00d9b
BPChance <bradenpchance6@gmail.com>
2025-01-27 17:06:50 -0600

    implement API authentication and secure endpoints

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,6 +14,14 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+auth = None
+auth_type = getenv('AUTH_TYPE')
+if auth_type:
+    if auth_type == 'auth':
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -31,6 +39,33 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """ forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_handler():
+    """
+    handles requests before they are handled,
+    pre handles requests
+    """
+    if auth is None:
+        return
+    
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    
+    auth_header = auth.authorization_header(request)
+    if auth_header is None:
+        abort(401)
+
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
 
 
 if __name__ == "__main__":
