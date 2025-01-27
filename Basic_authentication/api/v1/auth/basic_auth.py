44792e74b1e9bf3ba724a99e2e01c1f0098160b5
BPChance <bradenpchance6@gmail.com>
2025-01-27 17:18:06 -0600

    created class that inherits all methods from Auth and updated auth initialization

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" authentication module """
+
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that
+    inherits all methods from Auth
+    """
+    pass
