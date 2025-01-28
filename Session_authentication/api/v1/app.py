30fec7a37cbaf2e52ebb208441f70b6048e1c35b
BPChance <bradenpchance6@gmail.com>
2025-01-28 16:24:42 -0600

    excluded path

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -59,7 +59,8 @@ def before_request_handler():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
 
     if not auth.require_auth(request.path, excluded_paths):
@@ -72,6 +73,12 @@ def before_request_handler():
     request.current_user = auth.current_user(request)
     if request.current_user is None:
         abort(403)
+
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
+        abort(401)
 
 
 if __name__ == "__main__":
