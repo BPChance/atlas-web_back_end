13d80bf75086cd5f12d5d519025b2b8b3948d9f4
BPChance <bradenpchance6@gmail.com>
2025-01-28 15:57:40 -0600

    bad code, fixed code

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """ creates a Session ID for a user_id """
-        if user_id is None or isinstance(user_id, str):
+        if user_id is None or not isinstance(user_id, str):
             return None
         
         session_id = str(uuid.uuid4())
