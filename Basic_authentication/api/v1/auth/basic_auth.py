b1b6a69f20a0d3297f8cd5f247e20a02ef5e4c7b
BPChance <bradenpchance6@gmail.com>
2025-01-27 18:28:12 -0600

    added a method that returns the Base64 part of the Authorization header for BasicAuth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,10 @@ class BasicAuth(Auth):
     BasicAuth class that
     inherits all methods from Auth
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header for BasicAuth """
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
