64fc6bd9b9aad8d7d06c78290ed94a9815be922b
BPChance <bradenpchance6@gmail.com>
2025-01-27 19:43:06 -0600

    added try and except to handle error when search is not found

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -72,8 +72,12 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
+        
+        try:
+            users = User.search({"email": user_email})
+        except KeyError:
+            return None
 
-        users = User.search({"email": user_email})
         if not users or len(users) == 0:
             return None
 
