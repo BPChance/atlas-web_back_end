7b2b44f11e46dfba7a2f90b8889e1e60b83a3784
BPChance <bradenpchance6@gmail.com>
2025-01-29 14:28:56 -0600

    added destroy_session method that destroys user session

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -78,3 +78,10 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except Exception:
             return None
+ 
+    def destroy_session(self, user_id: int) -> None:
+        """ destroy user session """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            pass
