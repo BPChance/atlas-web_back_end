3d66339ab0b50992e851a1d1a37014d27868c55b
BPChance <bradenpchance6@gmail.com>
2025-01-29 14:15:30 -0600

    pycode

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -66,7 +66,7 @@ class Auth:
         session_id = self._generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
-    
+
     def get_user_from_session_id(self, session_id: str) -> User:
         """ get user based on session id """
         if session_id is None:
