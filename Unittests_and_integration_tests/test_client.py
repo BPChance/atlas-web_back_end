c18bf413ea49fc43f77ec23edb379340d48fdb46
BPChance <bradenpchance6@gmail.com>
2025-02-02 13:03:25 -0600

    testing GithubOrgClient to ensure it fetches org data

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" client.py testing """
+
+
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Test Github Org Client """
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name: str, mock_get_json: Mock) -> None:
+        """ test that it returns correct value """
+        test_payload = {"name": org_name}
+        mock_get_json.return_value = test_payload
+
+        client = GithubOrgClient(org_name)
+        result = client.org
+
+        self.assertEqual(result, test_payload)
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
