ceb59791193c7315066176c1a333c942a75f7f9a
BPChance <bradenpchance6@gmail.com>
2025-02-11 20:26:02 -0600

    Added Redis caching system with store and retrieval methods

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Any, Callable
 
 
 class Cache:
@@ -18,3 +18,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self, key: str, fn: Optional[callable[[bytes], any]] = None
+    ) -> Any:
+        """ retrieve data from redis """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+    
+    def get_str(self, key: str) -> Optional[str]:
+        """ retrieve a str from redis """
+        return self.get(key, lambda d: d.decode("utf-8"))
+    
+    def get_int(self, key: str) -> Optional[int]:
+        """ retrieve an int from redis """
+        return self.get(key, int)
