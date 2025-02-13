6a86a5a2a53424407bf8bd3e50acaf62e6bf9f59
BPChance <bradenpchance6@gmail.com>
2025-02-12 19:29:17 -0600

    replay function to display the history of calls of a particular function

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -37,6 +37,25 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """ displays the history of calls of a particular function """
+    # get redis instance from method class
+    redis_instance = method._self__._redis
+    # generate keys for input and output
+    inputs_key = f"{method.__qualname__}:inputs"
+    outputs_key = f"{method.__qualname__}:outputs"
+    # fetch the inputs and outputs
+    inputs = redis_instance.lrange(inputs_key, 0, -1)
+    outputs = redis_instance.lrange(outputs_key, 0, -1)
+    # get call count
+    call_count = redis_instance.get(method.__qualname__).decode("utf-8")
+    #display the history
+    print(f"{method.__qualname__} was called {call_count} times:")
+    for input_args, output in zip(inputs, outputs):
+        print(f"{method.__qualname__}(*{input_args.decode('utf-8')}) ->
+              {output.decode('utf-8')}")
+
+
 class Cache:
     """ cache class """
     def __init__(self):
