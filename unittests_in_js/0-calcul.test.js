41d0f47eb7cd36250db734014e3466c6d4d59661
BPChance <bradenpchance6@gmail.com>
2025-02-27 04:48:22 -0600

    pushing test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,8 +1,8 @@
 const calculateNumber = require('./0-calcul');
 const assert = require('assert').strict;
 
-describe(['calculateNumber test'], function() {
-    it(['should round the numbers and give their sum'], function() {
+describe('calculateNumber test', function() {
+    it('should round the numbers and give their sum', function() {
             assert.strictEqual(calculateNumber(1.4, 2.6), 4);
             assert.strictEqual(calculateNumber(5, 6), 11);
             assert.strictEqual(calculateNumber(-1.3, -2.7), -4);
